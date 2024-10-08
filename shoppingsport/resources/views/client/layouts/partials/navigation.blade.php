43b697b5ca413e11bd03484f2797a1bd05dadd08
datdev <dnguyentien145@gmail.com>
2024-10-08 15:51:41 +0700

    cart

--- shoppingsport/resources/views/client/layouts/partials/navigation.blade.php
+++ shoppingsport/resources/views/client/layouts/partials/navigation.blade.php
@@ -15,6 +15,14 @@
                 </svg>
             </span>
         </div>
+        <div class="search form-search flex-inline-center w-100" id="search_mobile">
+            <form method="GET" class="search-form w-100" action="https://thanhloisport.com/tim-kiem">
+                <input type="text" class="form-control w-100" aria-label="search" name="search" autocomplete="off" placeholder="Tìm kiếm sản phẩm..." required="" value="">
+                <button class="btn btn-search" type="submit" aria-label="btn search" name="btn_search">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="18"><path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"></path></svg>
+                </button>
+            </form>
+        </div>
         <p class="navigation-title flex-center-left">Menu</p>
         <nav class="navigation-nav">
             <ul class="navigation-nav__content">
